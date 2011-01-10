816b1961cc214835edfb269ff05e1db72ea7818f
vevisuke <vevisuke@gmail.com>
2011-01-10 22:46:55 +0900

    added module and test file
    外部ライブラリを追加していないのでこのコミットでは正常動作しない。

---
+++ bin/Diffuser.py
@@ -0,0 +1,11 @@
+# -*- coding=utf-8 -*-
+
+import sys
+sys.path.append('lib/')
+sys.path.append('vender/tweepy/')
+from tweetdiffuser import TweetDiffuser
+
+config_file = sys.argv[1]
+diffuser = TweetDiffuser(config_file)
+print diffuser.oauth()
+#diffuser.diffuse()
