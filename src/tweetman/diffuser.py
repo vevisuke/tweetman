f679e10e911790beb07659c51251411f01c8a224
vevisuke <vevisuke@gmail.com>
2011-02-01 15:27:34 +0900

    Diffuserクラスをリファクタリング
    テストも追加

--- src/tweetman/diffuser.py
+++ src/tweetman/diffuser.py
@@ -7,27 +7,54 @@ class Diffuser(Agent):
         sent_dm_list = []
         latest_id = self.db.get_latest_row().id if self.db.get_latest_row() else 0
         dm_list = self.api.direct_messages(since_id=latest_id)
+
         for dm in dm_list:
-            tweet_str = Message(dm.text)
-            message = tweet_str.create_cc_direct_message(dm.sender.screen_name)
-            sent_dm_list.append(self.send_direct_messages(message, self.get_dm_member_list()))
+            sent_dm_list.append(self._send_dm(dm))
+            self._send_dm_notice(dm)
+            self.db.write_row(dm.id, dm.sender.screen_name, dm.text)
 
-            notice = tweet_str.create_cc_direct_message_notice(self.get_dm_member_list())
-            self.send_direct_message(notice, dm.sender.screen_name)
-
-            self.db.add_row(dm.id, dm.sender.screen_name, '')
-        print sent_dm_list
         if len(sent_dm_list) == len(dm_list):
             return True
         else:
             return False
 
-    def get_dm_member_list(self):
+    def _send_dm(self, dm):
+        message = DirectMessage(dm)
+        text = message.create_cc_direct_message()
+        return self.send_direct_messages(text, self.get_dm_member_list(message.sender))
+
+    def _send_dm_notice(self, dm):
+        message = DirectMessage(dm)
+        text = message.create_cc_direct_message_notice(self.get_dm_member_list())
+        return self.send_direct_message(text, message.sender)
+
+    def get_dm_member_list(self, ignore=None):
         send_users = []
         for follower in self.api.followers():
-            if follower.screen_name in self.config.member_list:
+            if follower.screen_name == ignore:
+                pass
+            elif follower.screen_name in self.config.get('global', 'member_list'):
                 send_users.append(follower.screen_name)
         return send_users
+
+
+class DirectMessage:
+    def __init__(self, dm):
+        self.original_dm = dm
+        self.id = dm.id
+        self.sender = dm.sender.screen_name
+        self.text = dm.text
+
+    def get_140str(self, string):
+        return string[0:139]
+
+    def create_cc_direct_message(self):
+        message = '[from ' + self.sender +  '] ' + self.text
+        return self.get_140str(message)
+
+    def create_cc_direct_message_notice(self, dm_user_list):
+        message =  ' '.join(dm_user_list) + u'に以下のDMを送信しました。' + self.text
+        return self.get_140str(message)
 
 
 class Message:
--- test/test_tweetman_diffuser.py
+++ test/test_tweetman_diffuser.py
@@ -4,7 +4,41 @@ import sys
 sys.path.append('src')
 sys.path.append('src/vender/tweepy')
 import unittest
-from tweetman.diffuser import Message
+from tweetman.diffuser import Message, DirectMessage
+
+class DMMock:
+    def __init__(self):
+        self.id = 1111
+        self.sender = SenderMock()
+        self.text = 'this_is_mock'
+
+
+class SenderMock:
+    def __init__(self):
+        self.screen_name = 'mock_user'
+
+
+class TestDirectMessage(unittest.TestCase):
+    def test_new_01(self):
+        mock = DMMock()
+        dm = DirectMessage(mock)
+        self.assertEqual(mock.id, dm.id)
+        self.assertEqual(mock.sender.screen_name, dm.sender)
+        self.assertEqual(mock.text, dm.text)
+
+    def test_create_cc_direct_message_01(self):
+        mock = DMMock()
+        dm = DirectMessage(mock)
+        text = u'[from mock_user] this_is_mock'
+        self.assertEqual(text, dm.create_cc_direct_message())
+
+    def test_create_cc_direct_message_notice_01(self):
+        mock = DMMock()
+        dm = DirectMessage(mock)
+        members = ['to1', 'to2']
+        text = u'to1 to2に以下のDMを送信しました。this_is_mock'
+        self.assertEqual(text, dm.create_cc_direct_message_notice(members))
+
 
 class TestMessage(unittest.TestCase):
     def test_get_140str_01(self):
